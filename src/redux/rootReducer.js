0702afa306199ceee13ffaef4c4537bf7e4f1b06
“lkesko1” <lejla.kesko@hotmail.com>
2021-11-13 20:54:08 +0100

    wip

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import shoppingReducer from "./shopping/shopping-reducers";
 
 console.log("test", "i jos nesto");
+console.log("test", "i jos nesto");
 
 const rootReducer = combineReducers({
   shop: shoppingReducer,
