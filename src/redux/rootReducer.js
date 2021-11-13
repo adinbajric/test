de507eee7d55e56216a2105b03658a2542833610
Adin Bajric <63010916+adinbajric@users.noreply.github.com>
2021-11-13 20:24:00 +0100

    promjena 32

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import shoppingReducer from "./shopping/shopping-reducers";
 
-console.log("test");
+console.log("lejla");
 
 const rootReducer = combineReducers({
   shop: shoppingReducer,
