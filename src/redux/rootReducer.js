c468779499d69fc8f235a28b5f22ed1381b3cbad
“lkesko1” <lejla.kesko@hotmail.com>
2021-11-13 20:18:52 +0100

    Feat: Add log

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import shoppingReducer from "./shopping/shopping-reducers";
 
+console.log("test");
+
 const rootReducer = combineReducers({
   shop: shoppingReducer,
 });
